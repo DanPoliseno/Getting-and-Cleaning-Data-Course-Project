d76664d0afeaa520eb6293ac4a06997a16883fe1
Dan Poliseno <dpolise@udel.edu>
2020-01-13 04:35:47 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,3 +8,21 @@
 #4. Appropriately labels the data set with descriptive variable names.
 #5. From the data set in step 4, creates a second, independent tidy data set with the average of each 
 #variable for each activity and each subject.
+
+#Dataset Prepartion:
+#Download File
+packages <- c("data.table", "reshape2")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
+datasetUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(datasetUrl, destfile = paste0(getwd(), '/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'), method = "auto")
+unzip("getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",exdir = "dataset")
+
+# Load activity labels + features
+features <- fread(file.path(path, "UCI HAR Dataset/features.txt")
+                  , col.names = c("index", "feature_label"))
+activity_labels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt")
+                        , col.names = c("classlabels", "activity_label"))
+extract_features <- grep("(mean|std)\\(\\)", features[, feature_label])
+measurements <- features[extract_features, feature_label]
+measurements <- gsub('[()]', '', measurements)
+
